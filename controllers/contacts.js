60a6d6447ab6cdf538701394e7392147aeee4efb
sorayaskavinski <soraya1br@gmail.com>
2025-03-18 12:24:46 -0300

    create, update and delete

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -15,6 +15,7 @@ const getSingle = async (req, res) => {
     try {
         const contactId = new ObjectId(req.params.id);
         const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId }).toArray();
+            
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(result[0]);
     } catch (err) {
@@ -22,8 +23,60 @@ const getSingle = async (req, res) => {
     }
 };
 
+const createContact = async (req, res) => {  
+    const contact ={
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+        if (response.acknowledged){
+            res.status(204).send();
+        }
+        else{
+            res.status(500).json(response.error || 'Some error ocurred while opening the contact.');
+        }        
+};
+
+const updateContact = async (req, res) => {
+   
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact);
+        if (response.modifiedCount > 0){
+            res.status(204).send();
+        }
+        else{
+            res.status(500).json(response.error || 'Some error ocurred while updating the contact');
+        }
+};
+
+const deleteContact = async (req, res) => {   
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId });
+
+    if (response.deletedCount > 0){
+        res.status(204).send();
+    }
+    else{
+        res.status(500).json(response.error || 'Some error ocurred while deleting the contact');
+    }
+};
+
 
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-
 const app = express ();
 
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 
